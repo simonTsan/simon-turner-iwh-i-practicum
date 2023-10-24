9dfe9af9d68e655aa0c45453e0fa5d7c02e5a0d0
Simon Turner <simon.turner@sandigital.uk>
2023-10-24 17:45:58 +0100

    Styling changes

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
+const PRIVATE_APP_ACCESS = "pat-eu1-943a997f-633d-44eb-b153-7f9b9fb5e5c2";
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
@@ -71,14 +71,14 @@ app.get("/update-cobj", async (req, res) => {
         },
       };
 
-      const updateContact = `https://api.hubapi.com/crm/v3/objects/artists`;
+      const updateArtist = `https://api.hubapi.com/crm/v3/objects/artists`;
       const headers = {
         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
         "Content-Type": "application/json",
       };
 
       try {
-        await axios.patch(updateContact, update, { headers });
+        await axios.patch(updateArtist, update, { headers });
         res.redirect("back");
       } catch (err) {
         console.error(err);
